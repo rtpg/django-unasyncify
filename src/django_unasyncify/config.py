99e0d2579f9f36ff8d5ed1e5b991ea00d0da9f93
Raphael Gaschignard <raphael@rtpg.co>
2025-02-08 18:15:35 +1000

    Fill in more config parameters

--- src/django_unasyncify/config.py
+++ src/django_unasyncify/config.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 
 from pathlib import Path
+from textwrap import dedent
 import tomllib
 
 
@@ -11,6 +12,7 @@ class Config:
     attribute_renames: dict[str, str] = field(default_factory=lambda: {})
     # XXX maybe rename this one
     codegen_import_path: str = "MISSING_IMPORT_PATH"
+    # XXX rename this one as well, and not include the defaults here
     codegen_generators_path: str = "MISSING"
 
     def __post_init__(self):
@@ -18,20 +20,44 @@ class Config:
         # it. I have not come up with a good situation to not do this.
         self.attribute_renames.setdefault("IS_ASYNC", "False")
 
-
     @classmethod
     def from_project_path(cls, path) -> "Config":
         return load_config_from_project_path(path)
 
+    def codegen_template_path(self) -> Path:
+        return self.project_base / self.codegen_generators_path
+
+
 def load_config_from_project_path(project_path: str) -> Config:
     project_base = Path(project_path)
     pyproject_location = project_base / "pyproject.toml"
-    with pyproject_location.open("rb") as pyproject_file:
-        pyproject = tomllib.load(pyproject_file)
+    try:
+        with pyproject_location.open("rb") as pyproject_file:
+            pyproject = tomllib.load(pyproject_file)
+    except FileNotFoundError:
+        raise IOError(
+            f"Could not find a pyproject.toml file for the project (at {project_path})"
+        )
 
-    # XXX error handling
-    paths_to_visit_config = pyproject["tool"]["django_unasyncify"]["paths_to_visit"]
+    try:
+        unasyncify_config = pyproject["tool"]["django_unasyncify"]
+    except KeyError:
+        raise ValueError(
+            dedent(
+                """
+        Could not find the [tool.django_unasyncify] config section
+        inside the pyproject.toml file of your project. Is it missing?
+        """
+            )
+        )
+
+    paths_to_visit_config = unasyncify_config.get("paths_to_visit", ["."])
     paths_to_visit = [str(project_base / path) for path in paths_to_visit_config]
+
     return Config(
-        project_base=project_base, paths_to_visit=paths_to_visit, attribute_renames={}
+        project_base=project_base,
+        paths_to_visit=paths_to_visit,
+        attribute_renames=unasyncify_config.get("attribute_renames", {}),
+        codegen_generators_path=unasyncify_config["codegen_generators_path"],
+        codegen_import_path=unasyncify_config["codegen_import_path"],
     )
